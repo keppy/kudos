5e39cc92ebb28ab6854b232d65d17757234978eb
keppy <keppy.rb@gmail.com>
2015-03-14 11:50:36 -0700

    Handle missing attrs in kudo_serializer.
    Makes the client able to successfuly create a kudo for a user in the
    current user's org.

--- app/serializers/kudo_serializer.rb
+++ app/serializers/kudo_serializer.rb
@@ -2,7 +2,7 @@ class KudoSerializer < ActiveModel::Serializer
   attributes :id, :sender, :reciever, :content
 
   def sender
-    if email = Person.find(object.sender_id).email
+    if object.sender_id and email = Person.find(object.sender_id).email
       return email
     else
       return "none"
@@ -10,7 +10,7 @@ class KudoSerializer < ActiveModel::Serializer
   end
 
   def reciever
-    if email = Person.find(object.reciever_id).email
+    if object.reciever_id and email = Person.find(object.reciever_id).email
       return email
     else
       return "none"
